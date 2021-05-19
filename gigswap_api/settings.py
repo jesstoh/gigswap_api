e3242bcc277d43ad402016318c95a5662ad4ed50
jesstoh <jesstoh23@gmail.com>
2021-05-19 19:48:49 +0800

    Create database gigswap

--- gigswap_api/settings.py
+++ gigswap_api/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'gigswap_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gigswap',
+        'USER': 'gigswap',
+        'PASSWORD': 'gigswap',
+        'HOST': 'localhost'
     }
 }
 
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE gigswap;
+CREATE USER gigswapuser WITH PASSWORD 'gigswapuser';
+GRANT ALL PRIVILEGES ON DATABASE gigswap TO gigswapuser;
