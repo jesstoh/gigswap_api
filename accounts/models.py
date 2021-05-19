332d8f9e5ec8dbc049e3b24a84216832b200098a
jesstoh <jesstoh23@gmail.com>
2021-05-19 20:34:55 +0800

    Create user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    is_hirer = models.BooleanField(default=False)
+
+
+
