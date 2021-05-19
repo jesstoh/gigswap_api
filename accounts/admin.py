80c13f4078a4be8721612b8c6fb4d16b88def50f
jesstoh <jesstoh23@gmail.com>
2021-05-19 21:17:08 +0800

    Create HirerProfile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from accounts.models import User
+from accounts.models import User, HirerProfile
 
 # Register your models here.
+class HirerProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'company', 'country']
+
+
 admin.site.register(User)
+admin.site.register(HirerProfile, HirerProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     is_hirer = models.BooleanField(default=False)
+
+class HirerProfile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="hirer_profile")
+    company = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    postal_code = models.IntegerField(blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    contact = models.CharField(max_length=20, blank=True)
 
 
 
