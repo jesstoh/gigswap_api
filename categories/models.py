302aa05f6786ba5f4a2e41580b5fa2c3d4233ee8
jesstoh <jesstoh23@gmail.com>
2021-05-19 21:32:18 +0800

    Create Category model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, null=False)
+
