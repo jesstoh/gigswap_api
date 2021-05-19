b44758135a5bbdd024639f7645cd20b8c4e7058e
jesstoh <jesstoh23@gmail.com>
2021-05-19 22:33:51 +0800

    Accounts- Register user view working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from accounts.views import *
 
-app_name = "accounts"
+app_name = 'accounts'
 
 urlpatterns = [
-
+    path('register/', register_view, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+from rest_framework import exceptions
+from django.core.exceptions import ValidationError
+from accounts.models import User
+from accounts.serializers import UserSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def register_view(request):
+    if request.method == 'POST':
+        user = UserSerializer(data=request.data)
+        if user.is_valid(raise_exception=True):
+            user.save()
+            #to change to login later
+            return Response({'message': 'User registered'})
