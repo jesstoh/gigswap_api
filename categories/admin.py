e650a11d9e38d409f43e9901ef41658d20f7c80c
jesstoh <jesstoh23@gmail.com>
2021-05-19 21:46:06 +0800

    Admin register categories & subcategories

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from categories.models import Category, Subcategory
 
 # Register your models here.
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+
+admin.site.register(Category)
+admin.site.register(Subcategory, SubcategoryAdmin)
--- categories/models.py
+++ categories/models.py
@@ -6,7 +6,13 @@ class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, null=False)
 
+    def __str__(self):
+        return self.name
+
 class Subcategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="subcategories")
+
+    def __str__(self):
+        return self.name
