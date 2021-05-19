e5bfb51833c69eb3b0057e67597b6bebc95f7084
jesstoh <jesstoh23@gmail.com>
2021-05-20 01:11:52 +0800

    Set up all url patterns of gigs app

--- gigs/urls.py
+++ gigs/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from gigs.views import *
 
-app_name = "gigs"
 
 urlpatterns = [
-    
+    path('', index_view, name='gigs_index'),
+    path('recommended/', recommend_gig_view, name="recommend_gig"),
+    path('<uuid:id>/', show_view, name='show_gig'),
+    path('<uuid:id>/save/', save_view, name='save_gig'),
+    path('<uuid:id>/unsave/', unsave_view, name='unsave_gig'),
+    path('<uuid:id>/apply/', apply_view, name='apply_gig'),
+    path('<uuid:id>/withdraw/', withdraw_view, name='withdraw_gig'),
+    path('<uuid:id>/close/', close_view, name='close_gig'),
+    path('<uuid:id>/award/', award_view, name='award_gig'),
+    path('<uuid:id>/invite/', invite_view, name='invite_talent'),
+    path('<uuid:id>/hirer/', hirer_view, name='hirer_gigs_index')
+
 ]
--- gigs/views.py
+++ gigs/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_view(request):
+    pass
+
+def recommend_gig_view(request):
+    pass
+
+def show_view(request, id):
+    pass
+
+def save_view(request, id):
+    pass
+
+def unsave_view(request, id):
+    pass
+
+def apply_view(request, id):
+    pass
+
+def withdraw_view(request, id):
+    pass
+
+def close_view(request, id):
+    pass
+
+def award_view(request, id):
+    pass
+
+def invite_view(request, id):
+    pass
+
+def hirer_view(request, id):
+    pass
+
