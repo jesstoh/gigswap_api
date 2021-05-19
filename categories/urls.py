823020ebbff0fbfa01e584b9e9f5741e4cd6d116
jesstoh <jesstoh23@gmail.com>
2021-05-19 22:56:26 +0800

    Create all url patterns for categories app

--- categories/urls.py
+++ categories/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from categories.views import *
 
-app_name = "categories"
+app_name = 'categories'
 
 urlpatterns = [
-    
+    path('', categories_index, name='categories_index'),
+    path('categories/sub/', subcat_index, name='subcat_index'),
+    path('categories/:id/', categories_show, name='show_category'),
+    path('categories/sub/:id', subcat_show, name='show_subcat')
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def categories_index(request):
+    pass
+
+def subcat_index(request):
+    pass
+
+def categories_show(request, id):
+    pass
+
+def subcat_show(request, id):
+    pass
