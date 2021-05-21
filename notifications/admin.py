3bfe70c47f948e259bf4fb2ea3883e1b8ff2418d
jesstoh <jesstoh23@gmail.com>
2021-05-21 21:15:40 +0800

    Create Notification model

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from notifications.models import Notification
 
 # Register your models here.
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user',]
+
+admin.site.register(Notification, NotificationAdmin)
--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+from accounts.models import User
 
 # Create your models here.
+class Notification(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    title = models.CharField(max_length=255, null=False)#headline of message
+    message = models.TextField(null=False)
+    is_read = models.BooleanField(default=False) #Store if notification is read by user
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
