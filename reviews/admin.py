ab39bf36462b0ef8db9032395e6566ca6b444f68
jesstoh <jesstoh23@gmail.com>
2021-05-22 16:32:31 +0800

    Create model to review hirer

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from reviews.models import TalentReview
+from reviews.models import TalentReview, HirerReview
 
 # Register your models here.
 admin.site.register(TalentReview)
+admin.site.register(HirerReview)
--- reviews/models.py
+++ reviews/models.py
@@ -23,4 +23,19 @@ class TalentReview(models.Model):
     def __str__(self):
         return self.talent.username
 
+#Model for reviewing hirer
+class HirerReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    gig = models.OneToOneField(Gig,on_delete=models.CASCADE, related_name='hirer_review')
+    hirer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hirer_review') #hirer being reviewed
+    talent = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hirer_reviewed_talent') #talent that review hirer
+    rating = models.PositiveIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    payment_ontime = models.BooleanField(default=True)
+    scope = models.PositiveIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)]) #Clarity of scope
+    description = models.TextField(blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.hirer.username
