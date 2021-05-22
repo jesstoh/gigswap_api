6dffb8d630697aa2a7fb34a7377c555701ae6ffd
jesstoh <jesstoh23@gmail.com>
2021-05-22 10:42:01 +0800

    Create HirerFav model

--- hirers/admin.py
+++ hirers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from hirers.models import HirerFav
 
 # Register your models here.
+admin.site.register(HirerFav)
--- hirers/models.py
+++ hirers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
+from accounts.models import User, TalentProfile
 
 # Create your models here.
+class HirerFav(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hirer_fav')
+    saved = models.ManyToManyField(TalentProfile, related_name='talent_saved')
+
+    def __str__(self):
+        return self.user
