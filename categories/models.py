51c1a83e98110c10940db0e8c4e7912991bef42e
jesstoh <jesstoh23@gmail.com>
2021-05-28 08:53:28 +0800

    Change name field of Category model to be unique

--- categories/models.py
+++ categories/models.py
@@ -4,7 +4,7 @@ import uuid
 # Create your models here.
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, null=False)
+    name = models.CharField(max_length=255, null=False, unique=True)
 
     def __str__(self):
         return self.name
