11dd4311af4c183a205713fdb4050250d9bdc6ab
jesstoh <jesstoh23@gmail.com>
2021-06-01 17:43:20 +0800

    Add 2 method fiels in gig serializer to check if gig is reviewed

--- gigs/serializers.py
+++ gigs/serializers.py
@@ -5,6 +5,7 @@ from categories.models import Subcategory
 from accounts.models import User, HirerProfile, TalentProfile
 from accounts.serializers import UserSerializer, HirerProfileSerializer, TalentProfileSerializer
 from talents.models import TalentFav
+from reviews.models import HirerReview, TalentReview
 
 
 class GigSerializer(serializers.ModelSerializer):
@@ -13,18 +14,27 @@ class GigSerializer(serializers.ModelSerializer):
     subcategories = SubcategorySerializer(read_only=True, many=True)
     applicants = serializers.SerializerMethodField('get_applicants')
     poster_profile = serializers.SerializerMethodField('get_hirer_profile')
+    is_hirer_reviewed = serializers.SerializerMethodField('get_hirer_review')
+    is_talent_reviewed = serializers.SerializerMethodField('get_talent_review')
 
     # Get applicant id in a list and flatten into a list
     def get_applicants(self, obj):
         # applicants = obj.talent_applied.all().values_list('user__id', flat=True)
         applicant_ids = obj.talent_applied.all().values_list('user__id', flat=True)
-        applicant_profiles = TalentProfile.objects.filter(user__in=applicant_ids)
+        applicant_profiles = TalentProfile.objects.filter(
+            user__in=applicant_ids)
         return TalentProfileSerializer(applicant_profiles, many=True).data
 
     def get_hirer_profile(self, obj):
         return HirerProfileSerializer(obj.poster.hirer_profile).data
         # return obj.poster.hirer_profile.company
 
+    def get_hirer_review(self, obj):
+        return HirerReview.objects.filter(gig=obj).exists()
+
+    def get_talent_review(self, obj):
+        return TalentReview.objects.filter(gig=obj).exists()
+        
     class Meta:
         model = Gig
         fields = "__all__"
